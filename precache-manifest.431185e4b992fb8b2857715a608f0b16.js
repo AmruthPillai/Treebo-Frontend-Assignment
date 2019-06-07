cdbaa5c04c45120ed776bdd0055df1b852b4ae24
Amruth Pillai <im.amruth@gmail.com>
2019-06-07 06:15:12 +0530

    Updates

---
+++ precache-manifest.431185e4b992fb8b2857715a608f0b16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd8a104bf1803dd2fece3dc65206dfb",
+    "url": "/Treebo-Frontend-Assignment/index.html"
+  },
+  {
+    "revision": "553a5cf613da6a01ab02",
+    "url": "/Treebo-Frontend-Assignment/static/css/main.683a2eea.chunk.css"
+  },
+  {
+    "revision": "fc64f3a3a42f745ee845",
+    "url": "/Treebo-Frontend-Assignment/static/js/2.082ccb32.chunk.js"
+  },
+  {
+    "revision": "553a5cf613da6a01ab02",
+    "url": "/Treebo-Frontend-Assignment/static/js/main.325903e5.chunk.js"
+  },
+  {
+    "revision": "35a806f9ecfb4ac5c940",
+    "url": "/Treebo-Frontend-Assignment/static/js/runtime~main.1d5d36b1.js"
+  }
+]);
